016bace871069ef30d266094607b40a98669fa5d
orange <1067357662@qq.com>
2019-03-03 12:04:55 +0800

    init

---
+++ admin-web/src/main/java/com/orange/tbk/adminweb/AdminWebApplication.java
@@ -0,0 +1,13 @@
+package com.orange.tbk.adminweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminWebApplication.class, args);
+    }
+
+}
---
+++ build/src/main/java/com/orange/tbk/build/BuildApplication.java
@@ -0,0 +1,13 @@
+package com.orange.tbk.build;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildApplication.class, args);
+    }
+
+}
