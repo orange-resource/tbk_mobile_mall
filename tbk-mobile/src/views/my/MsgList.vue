331c28d1f88b11efd8a12a3b673c0cd5b4bc2539
orange <1067357662@qq.com>
2019-03-11 17:10:04 +0800

    增加后台页面

---
+++ tbk-mobile/src/views/my/MsgList.vue
@@ -0,0 +1,50 @@
+<template>
+
+    <div class="bc" style="height: 100%">
+
+        <div class="bc">
+
+            <!--头部-->
+            <van-nav-bar
+                    title="系统消息"
+                    left-text="返回"
+                    right-text="我的"
+                    left-arrow
+                    @click-left="onClickLeft"
+                    @click-right="onClickRight"
+            />
+            <!--头部 end-->
+
+            <orange-article-card
+                    v-for="(p,io) in 3"
+                    :key="io"
+                    title="资金管理"
+                    author="orange"
+                    date="2019.1.1"
+                    to="article"
+                    style="margin-top: 10px"
+            ></orange-article-card>
+
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+        name: "MsgList",
+        methods: {
+            onClickLeft() {
+                window.history.go(-1);
+            },
+            onClickRight() {
+                location.assign("/my");
+            },
+        },
+    }
+</script>
+
+<style scoped>
+</style>
